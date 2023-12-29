adc00e7906c3896d99984ef5fadbbbfa0acd2b31
Raemond <raemond.bw@gmail.com>
2023-12-29 16:38:28 -0500

    Initial Commit

---
+++ AppBase/AppBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppBaseApp.swift
+//  AppBase
+//
+//  Created by Raemond Bergstrom-Wood on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct AppBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
